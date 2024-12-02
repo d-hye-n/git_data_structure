bfb77a9e4186bac7a2dcb8efd9c9a0e971e53db8
kimdohyeon <qordkadkwla@gmail.com>
2024-12-02 14:15:19 +0900

    updated

--- P-6.py
+++ P-6.py
@@ -1,5 +1,6 @@
+# 코드 9.1: 이진탐색트리를 위한 노드 클래스
 class BSTNode:
-    def __init__(self, key, value=None):
+    def __init__ (self, key, value=None):
         self.key = key
         self.value = value
         self.left = None
@@ -8,9 +9,11 @@ class BSTNode:
     def isLeaf(self):
         return self.left is None and self.right is None
 
+#################################################################
 
-def search_bst(n, key):
-    if n is None:
+# 코드 9.2: 이진탐색트리의 탐색 연산(순환 구조)
+def search_bst(n, key) :
+    if n == None :
         return None
     elif key == n.key:
         return n
@@ -19,117 +22,131 @@ def search_bst(n, key):
     else:
         return search_bst(n.right, key)
 
+# 코드 9.3: 이진탐색트리의 탐색 연산(반복 구조)
+def search_bst_iter(n, key) :
+    while n != None :
+        if key == n.key:
+            return n
+        elif key < n.key:
+            n = n.left
+        else:
+            n = n.right
+    return None
 
-def search_value_bst(n, value):
-    if n is None:
+# 코드 9.4: 이진탐색트리의 값을 이용한 탐색 연산
+def search_value_bst(n, value) :
+    if n == None :
         return None
     elif value == n.value:
         return n
     res = search_value_bst(n.left, value)
-    if res is not None:
-        return res
+    if res is not None :
+       return res
     return search_value_bst(n.right, value)
 
 
-def search_max_bst(n):
-    while n is not None and n.right is not None:
+# 코드 9.5: 최대와 최소 키를 가지는 노드 탐색 연산
+def search_max_bst(n) :
+    while n != None and n.right != None:
         n = n.right
     return n
 
-
-def search_min_bst(n):
-    while n is not None and n.left is not None:
+def search_min_bst(n) :
+    while n != None and n.left != None:
         n = n.left
     return n
 
 
-def insert_bst(r, n):
+#################################################################
+
+# 코드 9.6: 이진탐색트리의 삽입 연산(순환 구조)
+def insert_bst(r, n) :
     if n.key < r.key:
-        if r.left is None:
-            r.left = n
-            return True
-        else:
-            return insert_bst(r.left, n)
-    elif n.key > r.key:
-        if r.right is None:
-            r.right = n
-            return True
-        else:
-            return insert_bst(r.right, n)
-    else:
+        if r.left is None :
+           r.left = n
+           return True
+        else :
+           return insert_bst(r.left, n)
+    elif n.key > r.key :
+        if r.right is None :
+           r.right = n
+           return True
+        else :
+           return insert_bst(r.right, n)
+    else :
         return False
 
+#################################################################
 
-def delete_bst_case1(parent, node, root):
-    if parent is None:
-        root = None
-    else:
-        if parent.left == node:
-            parent.left = None
-        else:
-            parent.right = None
+# 코드 9.7: 단말노드의 삭제 연산(case1)
+def delete_bst_case1 (parent, node, root) :
+    if parent is None: 			# 삭제할 단말 노드가 루트이면
+        root = None			    # 공백 트리가 됨
+    else :
+        if parent.left == node:	# 삭제할 노드가 부모의 왼쪽 자식이면
+            parent.left = None	# 부모의 왼쪽 링크를 None
+        else :				    # 오른쪽 자식이면
+            parent.right = None	# 부모의 오른쪽 링크를 None
     return root
 
+# 코드 9.8: 자식이 하나인 노드의 삭제 연산(case2)
+def delete_bst_case2 (parent, node, root) :
+    if node.left is not None :	# 삭제할 노드가 왼쪽 자식만 가짐
+        child = node.left		# child는 왼쪽 자식
+    else :				        # 삭제할 노드가 오른쪽 자식만 가짐
+        child = node.right		# child는 오른쪽 자식
 
-def delete_bst_case2(parent, node, root):
-    if node.left is not None:
-        child = node.left
-    else:
-        child = node.right
-
-    if node == root:
-        root = child
-    else:
-        if node == parent.left:
-            parent.left = child
-        else:
-            parent.right = child
+    if node == root :			# 없애려는 노드가 루트이면
+        root = child			# 이제 child가 새로운 루트가 됨
+    else :
+        if node is parent.left : 	# 삭제할 노드가 부모의 왼쪽 자식
+            parent.left = child		# 부모의 왼쪽 링크를 변경
+        else :				        # 삭제할 노드가 부모의 오른쪽 자식
+            parent.right = child	# 부모의 오른쪽 링크를 변경
     return root
 
-
-def delete_bst_case3(parent, node, root):
-    succp = node
-    succ = node.right
-    while succ.left is not None:
+# 코드 9.9: 자식이 둘인 노드의 삭제 연산(case2)
+def delete_bst_case3 (parent, node, root) :
+    succp = node			    # 후계자의 부모 노드
+    succ = node.right			# 후계자 노드
+    while (succ.left != None) :	# 후계자와 부모노드 탐색
         succp = succ
         succ = succ.left
 
-    if succp.left == succ:
-        succp.left = succ.right
-    else:
-        succp.right = succ.right
+    if (succp.left == succ) :	# 후계자가 왼쪽 자식이면
+        succp.left = succ.right	# 후계자의 오른쪽 자식 연결
+    else :				        # 후계자가 오른쪽 자식이면
+        succp.right = succ.right	# 후계자의 왼쪽 자식 연결
 
-    node.key = succ.key
-    node.value = succ.value
-    node = succ
+    node.key = succ.key			# 후계자의 키와 값을
+    node.value= succ.value		# 삭제할 노드에 복사
+    node = succ			        # 실제로 삭제하는 것은 후계자 노드
 
     return root
 
+# 코드 9.10: 이진탐색트리의 삭제 연산
+def delete_bst (root, key) :
+    if root == None : return None       # 공백 트리
 
-def delete_bst(root, key):
-    if root is None:
-        return None
-
-    parent = None
-    node = root
-    while node is not None and node.key != key:
+    parent = None                       # 삭제할 노드의 부모 탐색
+    node = root                         # 삭제할 노드 탐색
+    while node != None and node.key != key :
         parent = node
-        if key < node.key:
-            node = node.left
-        else:
-            node = node.right
+        if key < node.key : node = node.left
+        else : node = node.right;
+    if node == None : return root       # 삭제할 노드가 없음
 
-    if node is None:
-        return root
+	# case 1: 단말 노드
+    if node.left == None and node.right == None :
+        root = delete_bst_case1 (parent, node, root)
 
-    if node.left is None and node.right is None:
-        root = delete_bst_case1(parent, node, root)
+	#  case 2: 하나의 자식을 가진 노드
+    elif node.left==None or node.right==None :
+        root = delete_bst_case2 (parent, node, root)
 
-    elif node.left is None or node.right is None:
-        root = delete_bst_case2(parent, node, root)
-
-    else:
-        root = delete_bst_case3(parent, node, root)
+	#  case 3: 두 개의 자식을 가진 노드
+    else :
+        root = delete_bst_case3 (parent, node, root)
 
     return root
 
@@ -137,30 +154,17 @@ def delete_bst(root, key):
 def inorder(n):
     if n is not None:
         inorder(n.left)
-        print(n.key, end=' ')
+        print(n.key, end=' ')  # node의 key만 중위순회로 출력
         inorder(n.right)
 
 
-def preorder(n):
-    if n is not None:
-        print(n.key, end=' ')
-    preorder(n.left)
-    preorder(n.right)
-
-
-def postorder(n):
-    if n.is_not_None():
-        postorder(n.left)
-        postorder(n.right)
-        print(n.key, end=" ")
-
-
+# 코드 9.11: 이진탐색트리를 이용한 맵 클래스
 class BSTMap():
     def __init__(self):
         self.root = None
 
     def isEmpty(self):
-        return self.root is None
+        return self.root == None
 
     def findMax(self):
         return search_max_bst(self.root)
@@ -170,6 +174,7 @@ class BSTMap():
 
     def search(self, key):
         return search_bst(self.root, key)
+        # return search_bst_iter(self.root, key)
 
     def searchValue(self, key):
         return search_value_bst(self.root, key)
@@ -184,12 +189,39 @@ class BSTMap():
     def delete(self, key):
         self.root = delete_bst(self.root, key)
 
-    def display(self, msg='BSTMap:', order=1):
-        print(msg, end=" ")
-        if order == 1:
-            inorder(self.root)
-        if order == 2:
-            preorder(self.root)
-        if order == 3:
-            postorder(self.root)
+    def display(self, msg='BTSMap :'):
+        print(msg, end='')
+        inorder(self.root)
         print()
+
+
+# =========================================================
+#   - 이 파일이 직접 실행될 때에는 다음 문장들을 실행함.
+#   - 다른 파일에서 모듈로 불려지는 경우는 실행되지 않음.
+# =========================================================
+# 코드 9.12: 이진탐색트리를 이용한 맵 테스트 프로그램
+if __name__ == "__main__":
+    data = [35, 18, 7, 26, 12, 3, 68, 22, 30, 99]
+    value = ["삼오", "일팔", "영칠", "이육", "일이", "영삼", "육팔", "이이", "삼영", "구구"]
+
+    map = BSTMap()
+    map.display("[삽입 전] : ")
+    for i in range(len(data)):
+        map.insert(data[i], value[i])
+        map.display("[삽입 %2d] : " % data[i])
+
+    print('[최대 키] : ', map.findMax().key)
+    print('[최소 키] : ', map.findMin().key)
+    print('[탐색 26] : ', '성공' if map.search(26) != None else '실패')
+    print('[탐색 25] : ', '성공' if map.search(25) != None else '실패')
+    print('[탐색 일팔]:', '성공' if map.searchValue("일팔") != None else '실패')
+    print('[탐색 일칠]:', '성공' if map.searchValue("일칠") != None else '실패')
+
+    map.delete(3)
+    map.display("[삭제  3] : ")
+    map.delete(68)
+    map.display("[삭제 68] : ")
+    map.delete(18)
+    map.display("[삭제 18] : ")
+    map.delete(35)
+    map.display("[삭제 35] : ")
