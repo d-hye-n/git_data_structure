dbf6565fbd851b4002545713bd6e3f94de6f367d
kimdohyeon <qordkadkwla@gmail.com>
2024-12-05 00:02:43 +0900

    updated

--- P-8.py
+++ P-8.py
@@ -61,3 +61,66 @@ DFS2(adjacency_list, vertex[0], set())
 print()
 print("BFS: ", end='')
 BFS(adjacency_list, vertex[0])
+print()
+
+def bfs_component(graph, start_vertex, visited):
+    component = []
+    vertex_queue = queue.Queue()
+    vertex_queue.put(start_vertex)
+    visited.add(start_vertex)
+
+    while not vertex_queue.empty():
+        current_vertex = vertex_queue.get()
+        component.append(current_vertex)
+
+        for neighbor in graph[current_vertex]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                vertex_queue.put(neighbor)
+
+    return component
+
+
+# 전체 연결된 컴포넌트를 찾는 함수
+def connected_components(graph):
+    visited = set()
+    components = []
+
+    for vertex in graph:
+        if vertex not in visited:
+            component = bfs_component(graph, vertex, visited)
+            components.append(component)
+            print("BFS로 연결된 컴포넌트:", component)
+
+
+# 실험
+connected_components(adjacency_list)
+
+from queue import PriorityQueue
+
+
+def prim_spanning_tree(graph, start_vertex):
+    mst = []  # 스패닝 트리에 포함된 엣지를 저장할 리스트
+    visited = set()  # 방문한 정점을 저장할 집합
+    edges = PriorityQueue()  # 엣지를 우선순위 큐로 관리
+
+    def add_edges(vertex):
+        visited.add(vertex)
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                edges.put((1, vertex, neighbor))  # (임의의 가중치, 시작, 종료)
+
+    add_edges(start_vertex)
+
+    while not edges.empty():
+        weight, u, v = edges.get()  # 비용, 시작, 끝 정점
+        if v not in visited:
+            visited.add(v)
+            mst.append((u, v))
+            add_edges(v)
+
+    return mst
+
+# 예시로 주어진 시작 정점 'A'을 기준으로 스패닝 트리 구하기
+spanning_tree = prim_spanning_tree(adjacency_list, 'A')
+print("Spanning Tree:", spanning_tree)
